45db8223f8b6d74a7e3948657f8408ca525df324
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-06-22 13:57:48 +0100

    move styling to separate css file

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -1,43 +1,44 @@
 const Modal = ({ data, content, setSelectedContent, setShowModal }) => {
   const { name, url, description, date, type } = content;
   return (
-    <div style={styles.modal}>
-      <div style={styles.modalContent}>
-        <div style={styles.modalHeader}>
-          <h1 style={styles.modalTitle}>{name}</h1>
+    <div className="modal">
+      <div className="modalContent">
+        <div className="modalHeader">
+          <h1 className="modalTitle">{name}</h1>
           <button
-            style={styles.modalCloseButton}
+            className="modalCloseButton"
             onClick={() => setShowModal(false)}
           >
             close
           </button>
         </div>
-        <div style={styles.splitContainer}>
-          <div style={styles.modalImageContainer}>
-            <img style={styles.modalImage} src={url} alt={name} />
-            <p style={styles.modalDescriptionText}>{description}</p>
+        <div className="splitContainer">
+          <div className="modalImageContainer">
+            <img className="modalImage" src={url} alt={name} />
+            <p className="modalDescriptionText">{description}</p>
           </div>
-          <div style={styles.modalDescription}>
-            <div style={styles.modalRowData}>
-              <p style={styles.modalDescriptionText}>Date</p>
-              <p style={styles.modalDescriptionText}>{date}</p>
+          <div className="modalDescription">
+            <div className="modalRowData">
+              <p className="modalDescriptionText">Date</p>
+              <p className="modalDescriptionText">{date}</p>
             </div>
-            <div style={styles.modalRowData}>
-              <p style={styles.modalDescriptionText}>Type</p>
-              <p style={styles.modalDescriptionText}>{type}</p>
+            <div className="modalRowData">
+              <p className="modalDescriptionText">Type</p>
+              <p className="modalDescriptionText">{type}</p>
             </div>
           </div>
         </div>
-        <div style={styles.modalImagePicker}>
+        <div className="modalImagePicker">
           {data.map((image_data, index) => {
             return (
               <div
-                style={styles.imageContainer}
+                className="imageContainer"
+                key={index}
                 onClick={() => {
                   setSelectedContent(image_data);
                 }}
               >
-                <img style={styles.image} src={image_data.url} alt={name} />
+                <img className="image" src={image_data.url} alt={name} />
               </div>
             );
           })}
@@ -45,113 +46,6 @@ const Modal = ({ data, content, setSelectedContent, setShowModal }) => {
       </div>
     </div>
   );
-};
-
-const styles = {
-  modal: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(0,0,0,0.5)",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    zIndex: 1,
-  },
-  modalContent: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    alignContent: "center",
-    width: "50%",
-    backgroundColor: "#ffffff",
-    borderRadius: "10px",
-    padding: "20px",
-  },
-  splitContainer: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    gap: "10px",
-  },
-  modalHeader: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignContent: "center",
-    width: "100%",
-    marginBottom: "20px",
-  },
-  modalTitle: {
-    fontSize: "2rem",
-    fontWeight: "bold",
-    color: "#000000",
-    textShadow: "2px 2px 2px #ffffff",
-    textAlign: "left",
-  },
-  modalCloseButton: {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    color: "#000000",
-    textShadow: "2px 2px 2px #ffffff",
-    textAlign: "left",
-    cursor: "pointer",
-    border: "none",
-    backgroundColor: "transparent",
-  },
-  modalImageContainer: {
-    width: "100%",
-    height: "50%",
-    marginBottom: "20px",
-  },
-  modalImage: {
-    width: "100%",
-    height: "100%",
-    objectFit: "contain",
-  },
-  modalDescription: {
-    display: "flex",
-    flexDirection: "column",
-    width: "100%",
-  },
-  modalRowData: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    flexDirection: "row",
-    width: "100%",
-  },
-  modalDescriptionText: {
-    fontSize: "1rem",
-    fontWeight: "bold",
-    color: "gray",
-    fontStyle: "italic",
-    textShadow: "2px 2px 2px #ffffff",
-    textAlign: "left",
-  },
-  modalImagePicker: {
-    display: "flex",
-    flexDirection: "row",
-    maxWidth: "100%",
-    overflowX: "scroll",
-    gap: "10px",
-  },
-  image: {
-    width: "200px",
-    height: "200px",
-    objectFit: "cover",
-  },
-  imageContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    alignContent: "center",
-    cursor: "pointer",
-  },
 };
 
 export default Modal;
