f16accd0aa127b926f936584d62cedbea3a74951
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-06-22 13:59:53 +0100

    move styling to separate file and add scrollToSection feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,17 @@
-const Header = (props) => {
+const Header = () => {
+  const scrollToSection = () => {
+    const targetSection = document.getElementById("container");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    <header style={styles.header}>
-      <h1 style={styles.title}>{props.title}</h1>
-      <h2 style={styles.subtitle}>{props.subtitle}</h2>
+    <header className="header">
+      <h1 className="title">Photo Gallery</h1>
+      <h2 className="subtitle">A simple photo gallery app</h2>
+      <span onClick={scrollToSection} className="material-symbols-outlined">
+        expand_circle_down
+      </span>{" "}
     </header>
   );
-};
-
-const styles = {
-  header: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    alignContent: "center",
-    width: "100%",
-    backgroundColor: "lightblue",
-    marginBottom: "20px",
-  },
-  title: {
-    fontSize: "3rem",
-    fontWeight: "bold",
-    color: "#ffffff",
-    textShadow: "2px 2px 2px #000000",
-    textAlign: "left",
-  },
-  subtitle: {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    color: "#ffffff",
-    textShadow: "2px 2px 2px #000000",
-    textAlign: "left",
-  },
 };
 
 export default Header;
